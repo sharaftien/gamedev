7c6e95cf7b7616a10cdb6a24ecd020b1581211e3
sharaftien <charafke_b@hotmail.com>
2024-11-17 00:15:21 +0100

    sand tile background visible
    not at all okay but at least i can see sand

--- Almoravids/Game1.cs
+++ Almoravids/Game1.cs
@@ -4,6 +4,8 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Almoravids
 {
@@ -14,6 +16,13 @@ namespace Almoravids
 
         private Hero hero;
 
+        private Dictionary<Vector2, int> bg;
+        private Dictionary<Vector2, int> bnd;
+        private Dictionary<Vector2, int> well;
+
+        private Texture2D textureAtlas; 
+
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -23,6 +32,37 @@ namespace Almoravids
 
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+            bg = LoadMap("../../../Data/maps/level_0/00_sahara_Background.csv");
+            bnd = LoadMap("../../../Data/maps/level_0/00_sahara_Bounds.csv");
+            well = LoadMap("../../../Data/maps/level_0/00_sahara_Well.csv");
+
+        }
+
+        private Dictionary<Vector2, int> LoadMap(string filepath){
+            Dictionary<Vector2, int> result = new();
+
+            StreamReader reader = new(filepath);
+
+            int y = 0;
+            string line;
+            while ((line = reader.ReadLine()) != null) {
+
+                string[] items = line.Split(',');
+
+                for (int x = 0; x < items.Length; x++) {
+                    if (int.TryParse(items[x], out int value))
+                    {                    
+                        if (value>-1)
+                        {
+                            result[new Vector2(x, y)] = value;
+                        }
+                    }
+                }
+                
+                y++;
+            }
+            return result;
         }
 
         protected override void Initialize()
@@ -35,12 +75,14 @@ namespace Almoravids
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            textureAtlas = Content.Load<Texture2D>("tiles/Terrain");
+
             // Load textures for Hero
             Texture2D walkTexture = Content.Load<Texture2D>("tashfin");
             Texture2D idleTexture = Content.Load<Texture2D>("tashfin_idle");
             
             // Set up initial position for Hero
-            Vector2 startPosition = new Vector2((1280/2)-64, (720/2)-64); // Adjust starting position as needed (pixels ons screen)
+            Vector2 startPosition = new Vector2((800/2), (480/2)); // Adjust starting position as needed (pixels ons screen)
 
             // Initialize input reader
             IInputReader inputReader = new KeyboardReader(); 
@@ -70,7 +112,36 @@ namespace Almoravids
             // TODO: Add your drawing code here
 
             _spriteBatch.Begin();
+            
+
+
+            int display_tilesize = 64;
+            int num_tiles_per_row = 8;
+            int pixel_tilesize = 8;
+
+            foreach (var item in bg)
+            {
+                Rectangle drect = new(
+                    (int)item.Key.X * display_tilesize,
+                    (int)item.Key.Y * display_tilesize,
+                    display_tilesize,
+                    display_tilesize
+                    );
+
+                int x = item.Value % num_tiles_per_row;
+                int y = item.Value / num_tiles_per_row;
+                Rectangle src = new(
+                    x * pixel_tilesize,
+                    y * pixel_tilesize,
+                    pixel_tilesize,
+                    pixel_tilesize
+                    );
+
+                _spriteBatch.Draw(textureAtlas, drect, src, Color.White);
+            }
+
             hero.Draw(_spriteBatch);
+
             _spriteBatch.End();
 
             base.Draw(gameTime);
