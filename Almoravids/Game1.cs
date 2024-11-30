ab33ebd4b4db24d5d376d5e72403f9fbeb14cb55
sharaftien <charafke_b@hotmail.com>
2024-11-30 22:50:40 +0100

    lamtuni swordsman loads and approaches hero

--- Almoravids/Game1.cs
+++ Almoravids/Game1.cs
@@ -15,6 +15,7 @@ namespace Almoravids
         private SpriteBatch _spriteBatch;
         private Map map;
         private Hero hero;
+        private Sahara_Swordman swordman;
 
         public Game1()
         {
@@ -41,10 +42,20 @@ namespace Almoravids
             // Load Hero textures
             Texture2D walkTexture = Content.Load<Texture2D>("tashfin");
             Texture2D idleTexture = Content.Load<Texture2D>("tashfin_idle");
-            Vector2 startPosition = new Vector2((800 / 2 - 50), (480 / 2 - 50));
+
+            // Initialize the hero character first, so that it can be used as the target
+            Vector2 startPosition = new Vector2(800 / 2 - 50, 480 / 2 - 50);
             IInputReader inputReader = new KeyboardReader();
             hero = new Hero(idleTexture, walkTexture, startPosition, inputReader);
+
+            // Load swordman texture
+            Texture2D swordmanWalkTexture = Content.Load<Texture2D>("characters/lamtuni");
+
+            // Initialize the Swordman enemy and set hero as the target
+            swordman = new Sahara_Swordman(swordmanWalkTexture, swordmanWalkTexture, new Vector2(100, 100), hero);  // Pass the hero object as the target
         }
+
+
 
         protected override void Update(GameTime gameTime)
         {
@@ -53,7 +64,11 @@ namespace Almoravids
 
             map.Update(gameTime);
 
-            hero.Update(gameTime);  // Update hero (movement, animations, etc.)
+            // Update the hero
+            hero.Update(gameTime);
+
+            // Update the swordman (enemy)
+            swordman.Update(gameTime);  // Update enemy's position and behavior
 
             base.Update(gameTime);
         }
@@ -67,8 +82,11 @@ namespace Almoravids
             // Draw the map layers
             map.Draw(_spriteBatch);
 
-            // Draw hero
+            // Draw the hero
             hero.Draw(_spriteBatch);
+
+            // Draw the swordman (enemy)
+            swordman.Draw(_spriteBatch);  // Draw enemy
 
             _spriteBatch.End();
 
