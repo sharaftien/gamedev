e98f0557c977074395172d18cf496bb6c03a1d87
sharaftien <charafke_b@hotmail.com>
2025-06-18 11:07:00 +0200

    apply animationComponent to hero and character

--- Almoravids/Characters/Character.cs
+++ Almoravids/Characters/Character.cs
@@ -12,15 +12,12 @@ namespace Almoravids.Characters
     {
         Up, Down, Left, Right
     }
+
     public abstract class Character : IGameObject
     {
-        protected Texture2D idleTexture;
-        protected Texture2D walkTexture;
-        protected Animate walkAnimate;
-        protected Animate idleAnimate;
-        protected Direction currentDirection;
-
+        protected AnimationComponent AnimationComponent;
         protected Vector2 position;
+        protected bool isMoving;
 
         public Vector2 Position
         {
@@ -28,39 +25,15 @@ namespace Almoravids.Characters
             protected set { position = value; }
         }
 
-        protected bool isMoving = false;
-
-        public Character(Texture2D idleTexture, Texture2D walkTexture, Vector2 startPosition)
+        public Character(Texture2D texture, Vector2 startPosition)
         {
-            this.idleTexture = idleTexture;
-            this.walkTexture = walkTexture;
-            this.position = startPosition;
-
-            //hero looks down when game starts
-            currentDirection = Direction.Down;
-
-            // Initialize the animation
-            walkAnimate = new Animate(0.15);     //standard speed
-            idleAnimate = new Animate(3);    //still a bit weird, to be changed
-            SetupAnimations();
-        }
-
-        private void SetupAnimations()
-        {
-            walkAnimate.LoadFrames(walkTexture, Direction.Down, 8, 10); // 8 frames, row 10
-            walkAnimate.LoadFrames(walkTexture, Direction.Up, 8, 8);   // 8 frames, row 8
-            walkAnimate.LoadFrames(walkTexture, Direction.Left, 8, 9); // 8 frames, row 9
-            walkAnimate.LoadFrames(walkTexture, Direction.Right, 8, 11); // 8 frames, row 11
-
-            idleAnimate.LoadFrames(idleTexture, Direction.Up, 2, 0);   // 2 frames, row 0
-            idleAnimate.LoadFrames(idleTexture, Direction.Left, 2, 1); // 2 frames, row 1
-            idleAnimate.LoadFrames(idleTexture, Direction.Down, 2, 2); // 2 frames, row 2
-            idleAnimate.LoadFrames(idleTexture, Direction.Right, 2, 3); // 2 frames, row 3
+            AnimationComponent = new AnimationComponent(texture);
+            position = startPosition;
         }
 
         public virtual void Update(GameTime gameTime)
         {
-            // Can be overridden in inherited classes
+            // can be overridden in inherited classes 
         }
 
         public virtual void Move(Vector2 direction, GameTime gameTime)
@@ -69,26 +42,12 @@ namespace Almoravids.Characters
             position += direction * 2; // *2 -> twice as fast
 
             // determine direction
-            if (direction.X > 0) currentDirection = Direction.Right;
-            else if (direction.X < 0) currentDirection = Direction.Left;
-            else if (direction.Y > 0) currentDirection = Direction.Down;
-            else if (direction.Y < 0) currentDirection = Direction.Up;
-
-            if (isMoving)
-            {
-                walkAnimate.Update(gameTime, currentDirection);
-            }
-            else
-            {
-                idleAnimate.Update(gameTime, currentDirection);
-            }
+            AnimationComponent.Update(gameTime, direction);
         }
 
         public virtual void Draw(SpriteBatch spriteBatch)
         {
-            Texture2D currentTexture = isMoving ? walkTexture : idleTexture;
-            var currentAnimate = isMoving ? walkAnimate : idleAnimate;
-            spriteBatch.Draw(currentTexture, position, currentAnimate.CurrentFrame.SourceRectangle, Color.White);
+            AnimationComponent.Draw(spriteBatch, position);
         }
     }
 }
--- Almoravids/Characters/Hero.cs
+++ Almoravids/Characters/Hero.cs
@@ -11,41 +11,20 @@ using Almoravids.Input;
 
 namespace Almoravids.Characters
 {
-    public class Hero : Character, IGameObject
+    public class Hero : Character
     {
         private readonly IInputReader inputReader;
 
-        public Hero(Texture2D idleTexture, Texture2D walkTexture, Vector2 startPosition, IInputReader inputReader)
-            : base(idleTexture, walkTexture, startPosition)
+        public Hero(Texture2D texture, Vector2 startPosition, IInputReader inputReader)
+            : base(texture, startPosition)
         {
             this.inputReader = inputReader;
         }
 
         public override void Update(GameTime gameTime)
         {
-            // Read input for movement direction
             Vector2 direction = inputReader.ReadInput();
-            Move(direction, gameTime); // Call base.Move with gameTime
-
-            if (isMoving)
-            {
-                walkAnimate.Update(gameTime, currentDirection);
-            }
-            else
-            {
-                idleAnimate.Update(gameTime, currentDirection);
-            }
+            Move(direction, gameTime);
         }
-
-        public override void Draw(SpriteBatch spriteBatch)
-        {
-            //// Draw hero using inherited Draw method
-            //base.Draw(spriteBatch);
-            Texture2D currentTexture = isMoving ? walkTexture : idleTexture;
-            AnimationFrame currentFrame = isMoving ? walkAnimate.CurrentFrame : idleAnimate.CurrentFrame;
-
-            spriteBatch.Draw(currentTexture, position, currentFrame.SourceRectangle, Color.White);
-        }
-
     }
 }
