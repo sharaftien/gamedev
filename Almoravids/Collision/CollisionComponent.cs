5c479f707772869ec1c002da98c048ee47b06263
sharaftien <charafke_b@hotmail.com>
2025-06-19 22:08:10 +0200

    added collisionComponent

---
+++ Almoravids/Collision/CollisionComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almoravids.Collision
+{
+    public class CollisionComponent
+    {
+        private Rectangle _boundingBox;
+        private readonly float _width;
+        private readonly float _height;
+
+        public CollisionComponent(float width, float height)
+        {
+            _width = width;
+            _height = height;
+            _boundingBox = new Rectangle(0, 0, (int)width, (int)height);
+        }
+
+        public void Update(Vector2 position)
+        {
+            _boundingBox.X = (int)position.X;
+            _boundingBox.Y = (int)position.Y;
+        }
+
+        public Rectangle BoundingBox => _boundingBox; // for checking collisions later
+    }
+}
