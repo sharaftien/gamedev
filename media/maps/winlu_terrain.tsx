9254bcc6746d2cd67642e1989feddc29fb7b1d84
sharaftien <charafke_b@hotmail.com>
2025-06-26 21:52:12 +0200

    cleanup maps and tiled files

--- media/maps/winlu_terrain.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="tashfin_idle" tilewidth="64" tileheight="64" tilecount="8" columns="2">
- <image source="sprites/tashfin_idle.png" width="128" height="256"/>
-</tileset>
