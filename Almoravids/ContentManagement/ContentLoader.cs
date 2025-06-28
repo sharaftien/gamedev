34ead5299f7432ad2747d58fca416fc378907bb3
sharaftien <charafke_b@hotmail.com>
2025-06-28 13:43:37 +0200

    added ContentLoader

---
+++ Almoravids/ContentManagement/ContentLoader.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Almoravids.ContentManagement
+{
+    public class ContentLoader
+    {
+        private readonly ContentManager _contentManager;
+
+        public ContentLoader(ContentManager contentManager)
+        {
+            _contentManager = contentManager;
+        }
+
+        public SpriteFont LoadSpriteFont(string path)
+        {
+            return _contentManager.Load<SpriteFont>(path);
+        }
+
+        public TiledMap LoadTiledMap(string path)
+        {
+            return _contentManager.Load<TiledMap>(path);
+        }
+
+        public Texture2D LoadTexture2D(string path)
+        {
+            return _contentManager.Load<Texture2D>(path);
+        }
+    }
+}
