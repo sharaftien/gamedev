5d3271078f8552392144609a3b907ae187dbc7c8
sharaftien <charafke_b@hotmail.com>
2025-06-30 11:23:21 +0200

    added base class for levels

---
+++ Almoravids/Level/Level.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Almoravids.Level
+{
+    public abstract class Level
+    {
+        protected readonly ContentLoader _contentLoader; // store content loader
+        protected readonly GraphicsDevice _graphicsDevice; // store graphics device
+        protected Map _map; // store map
+        protected Vector2 _heroSpawn; // store hero spawn position
+        protected List<Vector2> _enemySpawns; // store enemy spawn positions
+        protected string _mapName; // store map name
+
+        protected Level(ContentLoader contentLoader, GraphicsDevice graphicsDevice)
+        {
+            _contentLoader = contentLoader; // initialize content loader
+            _graphicsDevice = graphicsDevice; // initialize graphics device
+        }
+
+        public abstract void Load(); // load level-specific data and map
+
+        public Map Map
+        {
+            get { return _map; }
+        }
+
+        public Vector2 HeroSpawn
+        {
+            get { return _heroSpawn; }
+        }
+
+        public List<Vector2> EnemySpawns
+        {
+            get { return _enemySpawns; }
+        }
+
+
+    }
+}
