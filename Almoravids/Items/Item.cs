45eedf991d30bdfd16ae9094718cda8b5db23fa1
sharaftien <charafke_b@hotmail.com>
2025-07-12 18:12:14 +0200

    added item (base) and koumiya class

---
+++ Almoravids/Items/Item.cs
@@ -0,0 +1,38 @@
+
+namespace Almoravids.Items
+{
+    public abstract class Item : IGameObject
+    {
+        public CollisionComponent CollisionComponent { get; protected set; }
+        protected readonly Texture2D _texture;
+        protected Vector2 _position;
+        protected bool _isActive;
+
+        protected Item(Texture2D texture, Vector2 position)
+        {
+            _texture = texture;
+            _position = position;
+            _isActive = true;
+            CollisionComponent = new CollisionComponent(48f, 48f, 8f, 8f); // 48x48 box met 8px offset
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            if (_isActive)
+            {
+                spriteBatch.Draw(_texture, _position, Color.White);
+            }
+        }
+
+        public bool IsActive => _isActive;
+
+        public virtual void OnPickup(Hero hero)
+        {
+            _isActive = false; // deactivate after pickup
+        }
+    }
+}
---
+++ Almoravids/Items/Koumiya.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Almoravids.Items
+{
+    public class Koumiya : Item
+    {
+        public Koumiya(Texture2D texture, Vector2 position)
+            : base(texture, position)
+        {
+        }
+
+        public override void OnPickup(Hero hero)
+        {
+            base.OnPickup(hero);
+        }
+    }
+}
