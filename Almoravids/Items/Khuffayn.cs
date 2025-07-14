2c1300e354de00846d2fcc54ddbf3e5bffcdf464
sharaftien <charafke_b@hotmail.com>
2025-07-14 13:05:40 +0200

    added classes for new powerups
    adarga (shield), khuffayn (sandals), litham (veil), tasbih (for HP)

---
+++ Almoravids/Items/Khuffayn.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Almoravids.Items
+{
+    public class Khuffayn : Item
+    {
+        public Khuffayn(Texture2D texture, Vector2 position)
+            : base(texture, position)
+        {
+        }
+
+        public override void OnPickup(Hero hero)
+        {
+            base.OnPickup(hero);
+            hero.AddItem("Khuffayn");
+        }
+    }
+}
---
+++ Almoravids/Items/Tasbih.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Almoravids.Items
+{
+    public class Tasbih : Item
+    {
+        public Tasbih(Texture2D texture, Vector2 position)
+            : base(texture, position)
+        {
+        }
+
+        public override void OnPickup(Hero hero)
+        {
+            base.OnPickup(hero);
+            hero.AddItem("Tasbih");
+        }
+    }
+}
