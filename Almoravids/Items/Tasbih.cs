1b3d9357642a9b249cf61d7441b2f77718a16090
sharaftien <charafke_b@hotmail.com>
2025-07-16 21:23:50 +0200

    added visual feedback when praying

--- Almoravids/Items/Tasbih.cs
+++ Almoravids/Items/Tasbih.cs
@@ -3,10 +3,14 @@ namespace Almoravids.Items
 {
     public class Tasbih : Item
     {
-        private const float ActivationDuration = 5f; 
         private Hero _hero;
+
+        private const float ActivationDuration = 5f; // 5 seconds to activate
         private bool _isActivating; // check if spatiebalk is being held
-        private float _activationTimer; // check for how long
+        private float _activationTimer; // check for how long          
+
+        private float _rotation;
+        private const float FullRotation = MathHelper.TwoPi;
 
         public Tasbih(Texture2D texture, Vector2 position)
             : base(texture, position)
@@ -14,6 +18,7 @@ namespace Almoravids.Items
             _activationTimer = 0f;
             _hero = null;
             _isActivating = false;
+            _rotation = 0f;
         }
 
         public override void OnPickup(Hero hero)
@@ -33,8 +38,10 @@ namespace Almoravids.Items
                     {
                         _isActivating = true;
                         _activationTimer = ActivationDuration;
+                        _rotation = 0f; // reset rotation
                     }
                     _activationTimer -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    _rotation = FullRotation * (1f - _activationTimer / ActivationDuration); // rotation lasts 5 seconds
                     Console.WriteLine($"Praying: {_activationTimer:F2} seconds remaining.");
                     if (_activationTimer <= 0f)
                     {
@@ -49,6 +56,22 @@ namespace Almoravids.Items
                     _isActivating = false;
                     Console.WriteLine("Stopped mid prayer.");
                 }
+            }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            if (_isActive)
+            {
+                base.Draw(spriteBatch); // draw on map when active
+            }
+            if (_isActivating && _hero != null)
+            {
+                // draw tasbih above hero
+                Vector2 heroScreenPosition = _hero.MovementComponent.Position;
+                Vector2 screenPosition = heroScreenPosition + new Vector2(32f, -10f);
+                Vector2 origin = new Vector2(_texture.Width / 2f, _texture.Height / 2f); // center of texture
+                spriteBatch.Draw(_texture, screenPosition, null, Color.White, _rotation, origin, 1f, SpriteEffects.None, 0f);
             }
         }
 
