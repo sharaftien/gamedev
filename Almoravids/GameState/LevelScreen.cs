89804ad68d31fbf2188c611ab8eac4885b12702f
sharaftien <charafke_b@hotmail.com>
2025-07-24 00:19:06 +0200

    implemented ButtonRenderer in LevelScreen

--- Almoravids/GameState/LevelScreen.cs
+++ Almoravids/GameState/LevelScreen.cs
@@ -8,57 +8,50 @@ namespace Almoravids.GameState
         private ContentLoader _contentLoader;
         private List<TextRenderer> _textRenderers;
         private Texture2D _background;
-
+        private List<ButtonRenderer> _buttons;
 
         public void Initialize(ContentManager content, GraphicsDevice graphicsDevice)
         {
             _graphicsDevice = graphicsDevice;
             _contentLoader = new ContentLoader(content);
+
             _font = _contentLoader.LoadSpriteFont("Fonts/Arial");
             _background = _contentLoader.LoadTexture2D("screens/finished");
 
-            // initialize text renderers
-            _textRenderers = new List<TextRenderer>
+            _buttons = new List<ButtonRenderer>
             {
-                new TextRenderer(_font, "Press [1] or [2] to select level", Color.White, 1.3f, true, true, _graphicsDevice),
-                new TextRenderer(_font, "Press [R] to return", Color.White, 1.3f, true, true, _graphicsDevice),
-                new TextRenderer(_font, "Level 1", Color.White, 1f, true, true, _graphicsDevice),
-                new TextRenderer(_font, "Level 2", Color.White, 1f, true, true, _graphicsDevice),
-                new TextRenderer(_font, "Level 3", Color.White, 1f, true, true, _graphicsDevice),
-                new TextRenderer(_font, "Return", Color.White, 1f, true, true, _graphicsDevice),
+                new ButtonRenderer(_font, "Sahara", Color.NavajoWhite, new Vector2(-230+16*4, 127+16*3), _graphicsDevice, () => // -228, 127 corner check
+                {
+                    GameStateManager.Instance.SetState(new GameplayScreen(1));
+                }),
+                new ButtonRenderer(_font, "Marrakech", Color.NavajoWhite, new Vector2(-88+16*5, -47), _graphicsDevice, () =>
+                {
+                    GameStateManager.Instance.SetState(new GameplayScreen(2));
+                }),
+                new ButtonRenderer(_font, "Tangiers", Color.NavajoWhite, new Vector2(-24+16*3, -143), _graphicsDevice, () =>
+                {
+                    GameStateManager.Instance.SetState(new GameplayScreen(3));
+                }),
+                new ButtonRenderer(_font, "Return", Color.Red, new Vector2(-406+16*4, -251), _graphicsDevice, () =>
+                {
+                    GameStateManager.Instance.SetState(new StartScreen());
+                })
             };
         }
 
         public void Update(GameTime gameTime)
         {
-            if (Keyboard.GetState().IsKeyDown(Keys.D1)|| Keyboard.GetState().IsKeyDown(Keys.NumPad1))
-            {
-                GameStateManager.Instance.SetState(new GameplayScreen(1));
-            }
-            if (Keyboard.GetState().IsKeyDown(Keys.D2) || Keyboard.GetState().IsKeyDown(Keys.NumPad2))
-            {
-                GameStateManager.Instance.SetState(new GameplayScreen(2));
-            }
-            if (Keyboard.GetState().IsKeyDown(Keys.D3) || Keyboard.GetState().IsKeyDown(Keys.NumPad3))
-            {
-                GameStateManager.Instance.SetState(new GameplayScreen(1));
-            }
-            if (Keyboard.GetState().IsKeyDown(Keys.R))
-            {
-                GameStateManager.Instance.SetState(new StartScreen());
-            }
+            var mouse = Mouse.GetState();
+            foreach (var b in _buttons)
+                b.Update(mouse);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Begin();
             spriteBatch.Draw(_background, Vector2.Zero, Color.White);
-            _textRenderers[0].Draw(spriteBatch, 0f, -200f);
-            _textRenderers[1].Draw(spriteBatch, 0f, -150f);
-            _textRenderers[2].Draw(spriteBatch, 0f, 0f);
-            _textRenderers[3].Draw(spriteBatch, 0f, 40f);
-            _textRenderers[4].Draw(spriteBatch, 0f, 80f);
-            _textRenderers[5].Draw(spriteBatch, 0f, 200f);
+            foreach (var button in _buttons)
+                button.Draw(spriteBatch);
             spriteBatch.End();
         }
     }
