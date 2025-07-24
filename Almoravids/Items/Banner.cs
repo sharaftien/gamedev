6377f0cc31feffbe81f4023961cdfabf186efbab
sharaftien <charafke_b@hotmail.com>
2025-07-24 14:15:12 +0200

    added banner textures and banner class

---
+++ Almoravids/Items/Banner.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Almoravids.Items
+{
+    public class Banner : Item
+    {
+        public Banner(Texture2D texture, Vector2 position)
+            : base(texture, position)
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+        }
+    }
+}
